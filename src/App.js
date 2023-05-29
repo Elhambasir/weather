bd109d9453c23b4ae745f26f663dcdcade8b0ec3
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-05-29 21:03:55 +0430

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import './App.css';
+import CityDetails from './components/CityDetails';
+import Home from './components/Home';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/details" element={<CityDetails />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
