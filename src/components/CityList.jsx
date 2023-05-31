9f7321ad436857db0e5d576773ca3037315df67f
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-05-31 12:41:44 +0430

    Update CityList.jsx
    Render html elements

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import PropTypes from 'prop-types';
 
-function CityList() {
+function CityList({ city, getWeatherImage }) {
   return (
-    <div>CityList</div>
+
+    <div className="mobile-wrapper-city-list-items">
+      <div className="mobile-wrapper-city-list-item1">
+        <img src={getWeatherImage(city.main.temp)} alt="sdfsdfa" width="50" />
+        <button type="button">
+          icon
+        </button>
+      </div>
+      <div className="mobile-wrapper-city-list-item2">
+        <h4 className="mobile-wrapper-city-list-item-name-text">{city.name}</h4>
+        <p className="mobile-wrapper-city-list-item-name-text">
+          {city.main.temp}
+          °C
+        </p>
+      </div>
+    </div>
   );
 }
 
+CityList.propTypes = {
+  city: PropTypes.string.isRequired,
+  getWeatherImage: PropTypes.string.isRequired,
+};
 export default CityList;
